57c49b6369a0f99549d3dd45d50068d4c7bd6e0a
Marcia Lee <marcia@columntax.com>
2022-07-12 18:19:53 -0700

    Update path from sdk to tax-filing

--- column-ios/UrlBuilder.swift
+++ column-ios/UrlBuilder.swift
@@ -7,9 +7,9 @@ class UrlBuilder {
         let jsonData = try? JSONSerialization.data(withJSONObject: params, options: [])
         let jsonString = String(data: jsonData!, encoding: .utf8)
         let base64Params = (jsonString!.data(using: .utf8)?.base64EncodedString())!
-        // Sandbox URL: "https://app-sandbox.columnapi.com/sdk?params="
-        // Production URL: "https://app.columnapi.com/sdk?params="
-        let urlString = "https://app-sandbox.columnapi.com/sdk?params=" + base64Params
+        // Sandbox URL: "https://app-sandbox.columnapi.com/tax-filing?params="
+        // Production URL: "https://app.columnapi.com/tax-filing?params="
+        let urlString = "https://app-sandbox.columnapi.com/tax-filing?params=" + base64Params
         let percentEncodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         return percentEncodedUrlString!
     }
